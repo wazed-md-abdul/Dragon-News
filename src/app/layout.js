f909f57f2b11634c4ff9501836f75319e1cec1d1
wazed-md-abdul <wazedjiad@gmail.com>
2026-04-25 14:27:21 +0800

    header navbar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/shared/Navbar";
+import { Header } from "@heroui/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,12 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Header />
+        <NavBar />
+        {children}
+
+        </body>
     </html>
   );
 }
