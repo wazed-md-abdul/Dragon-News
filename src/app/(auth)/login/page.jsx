a15cc28314a7bfa8d035fc6e93f90ef81262310a
wazed-md-abdul <wazedjiad@gmail.com>
2026-04-26 04:13:55 +0800

    login page and register page added

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,11 +1,79 @@
-import React from 'react';
+"use client";
+import {Check} from "@gravity-ui/icons";
+import {Button, Description, FieldError, Form, Input, Label, TextField} from "@heroui/react";
+import Link from "next/link";
 
-const LoginPage = () => {
-    return (
-        <div>
-            hi hello
-        </div>
-    );
-};
+export default function Login() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formDataObj = Object.fromEntries(new FormData(e.currentTarget));
+    console.log(formDataObj);
+  };
 
-export default LoginPage;
+  return (
+    <div className="container mx-auto my-50">
+        <h1 className=" text-lg font-bold text-center my-10 ">Login Your Account</h1>
+    <Form
+      className="flex w-96 flex-col gap-4 mx-auto my-auto"
+      render={(props) => <form {...props} data-custom="foo" />}
+      onSubmit={onSubmit}
+    >
+      <TextField
+        isRequired
+        name="email"
+        type="email"
+        validate={(value) => {
+          if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+            return "Please enter a valid email address";
+          }
+
+          return null;
+        }}
+      >
+        <Label>Email</Label>
+        <Input placeholder="Enter your email" />
+        <FieldError />
+      </TextField>
+
+      <TextField
+        isRequired
+        minLength={8}
+        name="password"
+        type="password"
+        validate={(value) => {
+          if (value.length < 8) {
+            return "Password must be at least 8 characters";
+          }
+          if (!/[A-Z]/.test(value)) {
+            return "Password must contain at least one uppercase letter";
+          }
+          if (!/[0-9]/.test(value)) {
+            return "Password must contain at least one number";
+          }
+
+          return null;
+        }}
+      >
+        <Label>Password</Label>
+        <Input placeholder="Enter your password" />
+        <Description>Must be at least 8 characters with 1 uppercase and 1 number</Description>
+        <FieldError />
+      </TextField>
+
+      <div className="flex gap-2 items-center justify-center">
+        <Button type="submit " className={"bg-blue-500"}>
+          <Check />
+          Login
+        </Button>
+        <Button type="reset" variant="danger">
+          Reset
+        </Button>
+          
+      </div>
+      <div className="text-center">
+        <p className="text-sm font-light text-gray-400">Dont have an account?</p><Link href="/register">Register</Link>
+            </div>
+    </Form>
+    </div>
+  );
+}
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,11 +1,117 @@
-import React from 'react';
+"use client";
+import { Check } from "@gravity-ui/icons";
+import { Button, Description, FieldError, Form, Input, Label, TextField, Checkbox } from "@heroui/react";
 
-const RegisterPage = () => {
+import Link from "next/link";
+
+export default function Login() {
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const formDataObj = Object.fromEntries(new FormData(e.currentTarget));
+        console.log(formDataObj);
+    };
+
     return (
-        <div>
-            
+        <div className="container mx-auto my-50">
+            <h1 className=" text-lg font-bold text-center my-10 ">Register Your Account</h1>
+            <Form
+                className="flex w-96 flex-col gap-4 mx-auto my-auto"
+                render={(props) => <form {...props} data-custom="foo" />}
+                onSubmit={onSubmit}
+            >
+                <TextField
+                    isRequired
+                    name="name"
+                    validate={(value) => {
+                        if (value.length < 3) {
+                            return "Name must be at least 3 characters";
+                        }
+                        return null;
+                    }}
+                >
+                    <Label>Name</Label>
+                    <Input placeholder="John Doe" />
+                    <FieldError />
+                </TextField>
+                <TextField isRequired
+                    name="imageUrl"
+                    type="text"
+                    validate={(value) => {
+                        if (!/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(value)) {
+                            return "Please enter a valid URL";
+                        }
+                        return null;
+                    }}
+                    >
+                    <Label>Image URL</Label>
+                    <Input placeholder="https://example.com/image.jpg" />
+                    <FieldError />
+
+
+                </TextField>
+                <TextField
+                    isRequired
+                    name="email"
+                    type="email"
+                    validate={(value) => {
+                        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                            return "Please enter a valid email address";
+                        }
+
+                        return null;
+                    }}
+                >
+                    <Label>Email</Label>
+                    <Input placeholder="Enter your email" />
+                    <FieldError />
+                </TextField>
+
+                <TextField
+                    isRequired
+                    minLength={8}
+                    name="password"
+                    type="password"
+                    validate={(value) => {
+                        if (value.length < 8) {
+                            return "Password must be at least 8 characters";
+                        }
+                        if (!/[A-Z]/.test(value)) {
+                            return "Password must contain at least one uppercase letter";
+                        }
+                        if (!/[0-9]/.test(value)) {
+                            return "Password must contain at least one number";
+                        }
+
+                        return null;
+                    }}
+                >
+                    <Label>Password</Label>
+                    <Input placeholder="Enter your password" />
+                    <Description>Must be at least 8 characters with 1 uppercase and 1 number</Description>
+                    <FieldError />
+                    <Checkbox id="basic-terms" className={"my-3"}>
+                        <Checkbox.Control>
+                            <Checkbox.Indicator />
+                        </Checkbox.Control>
+                        <Checkbox.Content>
+                            <Label htmlFor="basic-terms">Accept terms and conditions</Label>
+                        </Checkbox.Content>
+                    </Checkbox>
+
+                </TextField>
+
+                <div className="flex gap-2 items-center justify-center">
+                    <Button type="submit " className={"bg-green-500"}>
+                        <Check />
+                        Register
+                    </Button>
+                    <Button type="reset" variant="danger">
+                        Reset
+                    </Button>
+
+                </div>
+
+            </Form>
         </div>
     );
-};
-
-export default RegisterPage;
+}
