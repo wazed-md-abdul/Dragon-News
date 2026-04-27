6f1a93df683ca8b843facd26512c7bf0e8024ec5
wazed-md-abdul <wazedjiad@gmail.com>
2026-04-27 18:54:43 +0800

    env updated

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -10,12 +10,12 @@ export const auth = betterAuth({
     emailAndPassword: { 
     enabled: true, 
   },
-//   socialProviders: {
-//         google: { 
-//             clientId: process.env.GOOGLE_CLIENT_ID, 
-//             clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-//         }, 
-//     },
+  socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        }, 
+    },
     socialProviders: {
         github: { 
             clientId: process.env.GITHUB_CLIENT_ID , 
